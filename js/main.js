0665a02b3bd6830e4a450603d0bd0d96a63dd6bc
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-15 15:38:15 +0200

    Add html element - js conditional

--- js/main.js
+++ js/main.js
@@ -1,10 +1,48 @@
-// Visualizzare in pagina 5 numeri casuali. 
-// Da lì parte un timer di 30 secondi.
-// Dopo 30 secondi l’utente deve inserire, uno alla volta, i 
-// numeri che ha visto precedentemente, tramite il prompt().
-// Dopo che sono stati inseriti i 5 numeri, il software dice quanti 
-// e quali dei numeri da indovinare sono stati individuati.
+/** 
+- Visualizzare in pagina 5 numeri casuali. 
+- Da lì parte un timer di 30 secondi.
+- Dopo 30 secondi l’utente deve inserire, uno alla volta, i 
+numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti 
+e quali dei numeri da indovinare sono stati individuati.*/
 
 
 
-//creiamo un array e stampiamo
+ //Recupero dal Dom gli elementi 
+ //Genero 5 numeri random da inserire nel div.result-output
+ //Stampo su display i numeri per 30sec => in seguito li rendo "none" 
+ //chiedo all' utente per 5 volte di inserire i numeri da 1-100
+ //dico all'utente con un allert quanti numeri ha memorizzato 
+
+//Genero 5 numeri random da inserire nel div.result-output
+for ( let i = 1; i <= 5; i++){
+    document.getElementById(`num-${i}`).innerHTML = randomInt(1, 150);
+}
+let resultOutput = document.querySelector('#result-output');
+let userCorrectAnswer = 0 ;
+
+setTimeout(function() {
+    document.querySelector('#numbers-group').classList.add('d-none');
+    for ( let i = 1; i <= 5; i++){
+        let userAnswer = parseInt(prompt(`Inserisci numero ${i} `));
+        console.log(userAnswer);
+        if (document.getElementById(`num-${i}`).innerHTML == userAnswer){
+        userCorrectAnswer++;
+        resultOutput.innerHTML += ` Numero indovinato dallo user ${i} `
+        }
+    }
+    resultOutput.innerHTML += `User ha indovinato ${userCorrectAnswer} numbers`
+}, 3000);
+
+
+
+/**Function that creates a random integer between two values
+ * 
+ * @param {*} min minimum value of the random integer 
+ * @param {*} max maximum value of the random integer 
+ * @returns a random integer between a min and a max value
+ */
+function randomInt (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+} 
+
